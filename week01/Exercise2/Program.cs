771b248b206a59f9de6c4780331c120a26c8664c
Martin Lotero Arenas <martiloteroarenas@gmail.com>
2026-03-10 23:55:58 -0500

    excercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Write your grade percentage: ");
+        string grade = Console.ReadLine();
+        int gradeInteger = int.Parse(grade);
+        string letter;
+        if (gradeInteger > 90 || gradeInteger == 90)
+        {
+            letter = "A";
+        }
+        else if (gradeInteger > 80 || gradeInteger == 80)
+        {
+            letter = "B";
+        }
+        else if (gradeInteger > 70 || gradeInteger == 70)
+        {
+            letter = "C";
+        }
+        else if (gradeInteger > 60 || gradeInteger == 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        if (letter == "A" || letter == "B" || letter == "C")
+        {
+            Console.WriteLine($"Congrats your grade is {letter}, you pass the course!");
+        }
+        else
+        {
+            Console.WriteLine($"Your grade is {letter}, You lose the course but you can do it better next time!");
+        }
     }
 }
