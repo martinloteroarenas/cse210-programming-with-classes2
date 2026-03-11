53d271d98d5331eaa294c821362efaa1c51eb811
Martin Lotero Arenas <martiloteroarenas@gmail.com>
2026-03-11 17:10:26 -0500

    excercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        int guessInt;
+        do
+        {
+            Console.Write("Guess the magic number: ");
+            string guess = Console.ReadLine();
+            guessInt = int.Parse(guess);
+            if  (guessInt > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guessInt < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }        
+        } while (guessInt != magicNumber);
+
+        Console.WriteLine("THAT IS THE HIDDEN NUMBER!");
+    
     }
 }
