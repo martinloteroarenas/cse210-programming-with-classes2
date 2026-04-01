b194741ace198eedb9f899ee99e52bb28df18e34
Martin Lotero Arenas <martiloteroarenas@gmail.com>
2026-03-31 22:49:39 -0500

    learning activity w05

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        MathAssignment mathAssignment1 = new MathAssignment("section 5.5", "10-12", "pablo", "fractions");
+        Console.WriteLine(mathAssignment1.GetSummary() + mathAssignment1.GetHomeworkList());
+
+        WritingAssignment writingAssignment1 = new WritingAssignment("the book of mormon", "moroni", "church history");
+        Console.WriteLine($"{writingAssignment1.GetSummary()}");
+        Console.WriteLine($"{writingAssignment1.GetWritingInformation()}");
     }
-}
+}   
